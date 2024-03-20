ae2197a2d2ca1dc03b31ff08031936a211da23b0
Shivashish <70470449+sh1vvy@users.noreply.github.com>
2024-03-20 14:43:05 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
     return 'Hello'
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0', debug=True) 
